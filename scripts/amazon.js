55ecb34d1ab43b09268f88077b0917f8f58a90af
yethan <sebastian.cybul04@gmail.com>
2023-11-10 11:16:05 +0100

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -70,7 +70,6 @@ document.querySelectorAll('.js-add-to-cart')
                     matchingItem = item;
                 }
             });
-
             if(matchingItem){
                 matchingItem.quantity += 1;
             }else{
@@ -79,6 +78,13 @@ document.querySelectorAll('.js-add-to-cart')
                 quantity: 1
             });
             }
-            console.log(cart);
+
+            let cartQuantity = 0
+            cart.forEach((item) => {
+                cartQuantity += item.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
         });
 });
+
