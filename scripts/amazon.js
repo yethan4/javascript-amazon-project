49fc013b605e77156b5607cec70bef03435ab808
yethan <sebastian.cybul04@gmail.com>
2023-11-27 19:03:49 +0100

    added messages 2second

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -55,6 +55,8 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+const addedMessageTimeouts = {};
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
@@ -87,5 +89,22 @@ document.querySelectorAll('.js-add-to-cart')
 
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+    addedMessage.classList.add('added-to-cart-visible');
+
+    setTimeout(() => {
+        const previousTimeoutId = addedMessageTimeouts[productId];
+        if (previousTimeoutId){
+            clearTimeout(previousTimeoutId);
+        }
+        const timeoutId = setTimeout(()=>{
+            addedMessage.classList.remove('added-to-cart-visible');
+        },2000)
+
+        addedMessageTimeouts[productId] = timeoutId;
+        });
     });
   });
+
+  setInterval(() => { console.log(addedMessageTimeouts)},2)
