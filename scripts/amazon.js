f0b23060a0cc5d563aebb20edc7bb7587251e6c1
yethan <sebastian.cybul04@gmail.com>
2023-11-27 18:18:26 +0100

    use the destructuring shortcut to simplify it

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -59,7 +59,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
+      const {productId} = button.dataset;
       const productQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
 
       let matchingItem;
@@ -74,8 +74,8 @@ document.querySelectorAll('.js-add-to-cart')
         matchingItem.quantity += productQuantity;
       } else {
         cart.push({
-          productId: productId,
-          quantity: productQuantity
+          productId,
+          productQuantity
         });
       }
 
