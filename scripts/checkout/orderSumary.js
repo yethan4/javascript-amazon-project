6a985233a93256fb0e3bbf565f6dc68d22a876d7
yethan <sebastian.cybul04@gmail.com>
2024-01-31 16:13:32 +0100

    make payment summary interactive

--- scripts/checkout/orderSumary.js
+++ scripts/checkout/orderSumary.js
@@ -5,12 +5,7 @@ import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
-
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
-
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
 
@@ -127,6 +122,7 @@ export function renderOrderSummary(){
         container.remove();
 
         updateCartQuantity();
+        renderPaymentSummary();
       });
     });
 
@@ -166,6 +162,7 @@ export function renderOrderSummary(){
         );
         quantityLabel.innerHTML = newQuantity;
         updateCartQuantity();
+        renderPaymentSummary();
       });
     });
 
@@ -187,6 +184,7 @@ export function renderOrderSummary(){
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
 }
