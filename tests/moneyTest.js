2cce2caa5b91d335c3d130e74277b20f943d5f22
yethan <sebastian.cybul04@gmail.com>
2024-02-03 14:25:11 +0100

    create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,34 @@
+// situation = test case 
+// basic test cases - tests if the code is working
+// edge cases - test with values that are tricky
+
+import {formatCurrency} from '../scripts/utils/money.js'
+
+// basic test
+console.log('test suite: fromatCurrency')
+
+console.log('converts cents into dollars');
+
+if (formatCurrency(2095) === '20.95'){
+  console.log('passed');
+}else{
+  console.log('failed');
+}
+
+// edge case
+console.log('works with 0');
+
+if(formatCurrency(0) === '0.00'){
+  console.log('passed');
+}else{
+  console.log('failed');
+}
+
+// edge cases 
+console.log('rounds up to the nearest cent');
+
+if(formatCurrency(2000.5) === '20.01'){
+  console.log('passed');
+}else{
+  console.log('failed');
+}
